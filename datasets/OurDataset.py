72e9242a1915ebade8cc1eb4f957dc870c401268
Mat Ruiz <matruizofficial@gmail.com>
2021-02-01 20:33:19 -0800

    qf4

--- datasets/OurDataset.py
+++ datasets/OurDataset.py
@@ -10,13 +10,13 @@ class OurDataset(torch.utils.data.Dataset):
     # want to access already downloaded items w/o loading them all into memory. Do this in __getitem__.
 
     def __init__(self):  # load pics from memory
-        train_set = pd.read_csv("../../train.csv")
+        train_set = pd.read_csv("../train.csv")
         self.pictures = train_set['image_id'].astype('string')
         self.disease_labels = train_set['label']
 
     # process images here, could augment pictures here to save memory
     def __getitem__(self, index):
-        img = mpimg.imread("../../train_images/"+self.pictures[index])
+        img = mpimg.imread("../train_images/"+self.pictures[index])
         trans = transforms.ToTensor()
         img=trans(img) 
         
